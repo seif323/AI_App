2cbd6967b0a8aef7807e7c660f87645f2484de3b
Seif Elboghdady <seifelboghdady@std.mans.edu.eg>
2025-08-19 13:05:53 +0300

    i am add schema for user and session

---
+++ Models/session.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+    userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",   // Reference to User schema
+    required: true
+  },
+  sessionStart: {
+    type: Date,
+    required: true
+  },
+  sessionEnd: {
+    type: Date,
+    required: true
+  },
+  focusLevel: {
+    type: [Number],  // Array of numbers (ex: focus level per minute)
+    default: []
+  },
+  feedback: {
+    type: String
+  },
+  rawData: {
+    type: Object // or [Object] depending on ML team needs
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.module("Session", sessionSchema);
---
+++ Models/user.model.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: /.+\@.+\..+/
+    },
+    password: {
+        type: String,
+        required: true,
+        select : false
+    },
+    firstName: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    DOB: {
+        type: Date
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    refreshToken: {
+        type: String
+    },
+    profileImage: {
+        type: String, // store image URL
+        default: "https://example.com/default-avatar.png"
+}
+
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  let salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('User', userSchema);
